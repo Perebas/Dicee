c9909c10844f06df65b6795d75280a118661ccc8
Wendel Santos <97599161+Perebas@users.noreply.github.com>
2022-03-05 07:36:27 -0300

    Add Dicee game files

---
+++ index.js
@@ -0,0 +1,33 @@
+// Random Number Not Abbreviated
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+// Random Number Abbreviated
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+// Result
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = " Player 2 wins!🚩";
+} 
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
