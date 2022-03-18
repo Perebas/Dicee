f9ad3ff02b8c42274f32cff73a5a64ee53138043
Wendel Santos <97599161+Perebas@users.noreply.github.com>
2022-03-18 15:49:21 -0300

    A space was missing in the second flag

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
 }
 else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = " Player 2 wins!🚩";
+    document.querySelector("h1").innerHTML = " Player 2 wins! 🚩";
 } 
 else {
     document.querySelector("h1").innerHTML = "Draw!";
